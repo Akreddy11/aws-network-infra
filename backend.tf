582f8c4107fe8c0f958b63cf4a2ded3feed872fd
Akreddy11 <153039770+Akreddy11@users.noreply.github.com>
2024-09-05 11:51:22 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-information-btc"
+    bucket         = "terraform-state-information1-btc"
     region         = "us-east-1"
     key            = "state/terraform.tfstate"
     dynamodb_table = "dynamodb-state-information"
