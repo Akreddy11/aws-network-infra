f26496e64cc8238998d139508a4f9914c3822855
Akreddy11 <153039770+Akreddy11@users.noreply.github.com>
2025-08-31 14:01:45 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,16 +1,38 @@
-variable "AWS_REGION" {
-  type    = string
-  default = "us-east-1"
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "az" {
+  description = "Availability Zone for subnets"
+  type        = string
+  default     = "us-east-1a"
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR Block"
+  description = "CIDR for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
-  description = "Public Subnet CIDR"
+  description = "CIDR for public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
 }
 
 variable "private_subnet_cidr" {
-  description = "Private Subnet CIDR"
+  description = "CIDR for private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "tags" {
+  description = "Common tags"
+  type        = map(string)
+  default = {
+    Environment = "production"
+    ManagedBy   = "terraform"
+  }
 }
